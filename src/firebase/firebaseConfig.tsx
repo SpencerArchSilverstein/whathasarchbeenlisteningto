aeb857b4819edeaa65c485d1399574886bb8b82b
MusicTastingClubNU <musictastingclub@gmail.com>
2025-09-01 17:26:52 -0400

    Refreshing firebase credentials

--- src/firebase/firebaseConfig.tsx
+++ src/firebase/firebaseConfig.tsx
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCu9MDIggIZhfbdIZFF-PTIzUkkykqK4cQ",
-  authDomain: "whathasarchbeenlisteningto.firebaseapp.com",
-  databaseURL: "https://whathasarchbeenlisteningto-default-rtdb.firebaseio.com",
-  projectId: "whathasarchbeenlisteningto",
-  storageBucket: "whathasarchbeenlisteningto.firebasestorage.app",
-  messagingSenderId: "714913698641",
-  appId: "1:714913698641:web:cf1f38d4d9483fe0e0f970",
-  measurementId: "G-WV9SNMXNTL",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
